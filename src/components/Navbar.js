2cfcb0e7212330fbc83b529bcbe0d4132d564b57
Julio Navarro <jnavarro@able.co>
2019-08-11 19:30:09 -0500

    Initial setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Navbar = props => {
+    return <div className="bg-teal-400 w-full h-10">
+        test
+    </div>;
+};
+
+Navbar.propTypes = {};
+
+export default Navbar;
